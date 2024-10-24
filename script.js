677886401f2037eceaddf7832cf1ab22746558a6
James Robertson <jaarobertson92@gmail.com>
2024-10-24 15:53:26 +0100

    basic html set up

--- script.js
+++ script.js
@@ -1,38 +1,35 @@
-let a = 0
-let b = 0
-let operator =""
-
+let a = 0;
+let b = 0;
+let operator = "";
 
 function add(a, b) {
-    return a + b
+  return a + b;
 }
 
 function subtract(a, b) {
-    return a - b
+  return a - b;
 }
 
 function multiply(a, b) {
-    return a * b
+  return a * b;
 }
 
 function divide(a, b) {
-    if (b == 0) {
-        return "ERROR: cannot divide by zero"
-    }
-    return a / b
+  if (b == 0) {
+    return "ERROR: cannot divide by zero";
+  }
+  return a / b;
 }
 
 function operate(a, b, operator) {
-    switch (operator) {
-        case "+":
-            return add(a, b);
-        case "-":
-            return subtract(a, b);
-        case "*":
-            return multiply(a, b);
-        case "/":
-            return divide(a, b)
-    }
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+  }
 }
-
-
