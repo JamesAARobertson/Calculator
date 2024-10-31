ff063d8e527db6b579f27abd2f4faf4d06297696
James Robertson <jaarobertson92@gmail.com>
2024-10-31 11:59:05 +0000

    equals button operation

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let operator = "";
 let displayValue = "";
 const display = document.getElementById("display");
 const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
 
 // ADD
 function add(a, b) {
@@ -39,12 +40,33 @@ function operate(a, b, operator) {
       return multiply(a, b);
     case "/":
       return divide(a, b);
+    default:
+      return "Invalid Operation";
   }
 }
 
-numberButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        displayValue += button.innerText;
-        display.value = displayValue
-    })
-})
+// handling number buttons for display
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    displayValue += button.innerText;
+    display.value = displayValue;
+  });
+});
+
+// handling operators
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.innerText === "=") {
+      b = parseFloat(displayValue);
+      displayValue = operate(a, b, operator).toString();
+      display.value = displayValue;
+      a = parseFloat(displayValue);
+      operator = "";
+    } else {
+      a = parseFloat(displayValue);
+      operator = button.innerText;
+      displayValue = "";
+      display.value = displayValue;
+    }
+  });
+});
